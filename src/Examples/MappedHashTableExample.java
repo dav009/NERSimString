7013698d7c2255a80e9847ea82162349422c436a
David Przybilla <dav.alejandro@gmail.com>
2012-07-11 16:32:01 +0200

    fixed format of Examples

--- src/Examples/MappedHashTableExample.java
+++ src/Examples/MappedHashTableExample.java
@@ -24,24 +24,24 @@ public class MappedHashTableExample {
             
                  //specifies the number of n-grams : 3
                  //the output file of the dictionary: dbDav.test
-                 //LowLevelHashMappedDictionary2 dimp=new LowLevelHashMappedDictionary2(3,"/home/attickid/testOutput/dbDav.test");
+                 LowLevelHashMappedDictionary2 dimp=new LowLevelHashMappedDictionary2(3,"/home/attickid/testOutput/dbDav.test");
                  
                  //wrapping the lowlevel implementation
-                 //DictionaryGenerator d1=new DictionaryImplementation(dimp);
+                 DictionaryGenerator d1=new DictionaryImplementation(dimp);
                  
                  //tells where is the dictionary that one wants to process
                  //the number of n-grams 
                  //the dictonayImplementation that is incharged of making it
-                 //DictionaryReader.createDictionaryFromCorpus("/home/attickid/ned.train2",3,d1);
+                DictionaryReader.createDictionaryFromCorpus("/home/attickid/ned.train2",3,d1);
                  //saves the dictionary into memeory
-                 //dimp.commit();
+                 dimp.commit();
        
            
                 //calls the search algorithm 
                 //it takes as an input the dictionaryImplemntation
                 //the disered mesaure
                 // the query
-                //SimString.search(d1, MeasureFactory.create(MeasureFactory.COSINE_SIMILARITY, 0.8), "samen Adv O");
+                SimString.search(d1, MeasureFactory.create(MeasureFactory.COSINE_SIMILARITY, 0.8), "samen Adv O");
            
         
             /* LOADING AND QUERYING A DICTIONARY */    
